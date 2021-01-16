fec52013f0fb051bf39045efb195e79db63a3475
JoseLucasapp <josluc2016@gmail.com>
2021-01-16 18:34:03 -0300

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const http = require('http').createServer(app);
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
@@ -15,4 +16,4 @@ app.get('/about', (req,res)=>{
     res.render('about');
 });
 
-app.listen(PORT);
+http.listen(PORT);
