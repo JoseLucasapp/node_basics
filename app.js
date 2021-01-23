472e56362051c09998918952d2eb99fa087848c1
JoseLucasapp <josluc2016@gmail.com>
2021-01-23 11:10:54 -0300

    add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./views'))
 
@@ -8,4 +9,4 @@ app.get('/',(req,res)=>{
     res.render(path.join(__dirname + "index.html"));
 });
 
-app.listen(3000);
+app.listen(PORT);
