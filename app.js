8f1022ab953f1b5497784ea406d78675f36de2f0
JoseLucasapp <josluc2016@gmail.com>
2021-01-23 11:08:28 -0300

    add

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const PORT = process.env.PORT || 3000;
+const path = require('path');
 const app = express();
-const http = require('http').createServer(app);
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.use(express.static('./views'))
 
-app.use(express.static('./public'))
-
-app.get('/', (req, res)=>{
-    res.render('home');
-});
-app.get('/about', (req,res)=>{
-    res.render('about');
+app.get('/',(req,res)=>{
+    res.render(path.join(__dirname + "index.html"));
 });
 
-http.listen(PORT);
+app.listen(3000);
